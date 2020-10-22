30e5b646d30aeff8167edec4550709b6836dfe69
Ekaterina Prusakova <eprusako@c1r6p10.hive.fi>
2020-10-22 11:40:27 +0300

    starting project

--- fdf.c
+++ fdf.c
@@ -1,22 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   fdf.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: eprusako <eprusako@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/10/20 19:53:31 by eprusako          #+#    #+#             */
-/*   Updated: 2020/10/22 11:38:37 by eprusako         ###   ########.fr       */
+/*   Created: 2020/10/22 11:38:49 by eprusako          #+#    #+#             */
+/*   Updated: 2020/10/22 11:38:50 by eprusako         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-
-int     main(int argc, char **argv)
-{
-
-}
